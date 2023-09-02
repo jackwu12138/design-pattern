abd4d469171354f8703676941f3e37c939c58c8b
wujunlong <2280284307@qq.com>
2023-09-02 15:25:17 +0800

    feat: 适配器模式(Adapter Pattern)

---
+++ src/main/java/com/example/adapter/MediaPlayer.java
@@ -0,0 +1,9 @@
+package com.example.adapter;
+
+/**
+ * @author jackwu
+ */
+public interface MediaPlayer {
+
+    public void play(String audioType, String fileName);
+}
---
+++ src/main/java/com/example/adapter/Mp4Player.java
@@ -0,0 +1,17 @@
+package com.example.adapter;
+
+/**
+ * @author jackwu
+ */
+public class Mp4Player implements AdvancedMediaPlayer {
+
+    @Override
+    public void playVlc(String fileName) {
+        // 什么也不做
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        System.out.println("Playing mp4 file. Name: " + fileName);
+    }
+}
